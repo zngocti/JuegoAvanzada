59ae0dc23f54aef5cef2de9d2599ddb034ceb8f1
zngocti <octi124@hotmail.com>
2017-11-18 13:02:10 -0300

    Prueba de sfml cargando un sprite

--- Proyecto Juego/Proyecto Juego/Source.cpp
+++ Proyecto Juego/Proyecto Juego/Source.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 #include "vld.h"
+#include <SFML/Graphics.hpp>
 
-void main()
+int main()
 {
-	std::cout << "Probando" << std::endl;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+	sf::Texture texture;
 
-	int* asd = new int(5);
+	// cargo un rectangulo de 64x64 que inicia en el (0, 0) del archivo
+	if (!texture.loadFromFile("../Assets/tile_01.png", sf::IntRect(0, 0, 64, 64)))
+	{
+		std::cout << "No cargo la imagen" << std::endl;
+	}
+	else
+	{
+		sf::Sprite sprite;
+		sprite.setTexture(texture);
+
+		while (window.isOpen())
+		{
+			sf::Event event;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+					window.close();
+			}
+			window.clear();
+			window.draw(sprite);
+			window.display();
+		}
+	}
+
+	return 0;
 }
