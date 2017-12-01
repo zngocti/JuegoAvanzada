14fac46c90f6e3c175df8398f2a928033037dd45
zngocti <octi124@hotmail.com>
2017-12-01 16:13:08 -0300

    Probando rapidxml

--- Proyecto Juego/Proyecto Juego/Source.cpp
+++ Proyecto Juego/Proyecto Juego/Source.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include "vld.h"
 #include <SFML/Graphics.hpp>
+#include "rapidxml.hpp"
+#include "rapidxml_utils.hpp"
 
 void main()
 {
+	rapidxml::file<> xmlFile("../Assets/ships_sheet.xml");
+	rapidxml::xml_document<> doc;
+	doc.parse<0>(xmlFile.data());
+
+	std::cout << "Nombre: " << doc.first_node()->name() << std::endl;
+	std::cout << "Valor: " << doc.first_node()->value() << std::endl;
+	std::cout << "Primer Atributo Nombre: " << doc.first_node()->first_attribute()->name() << std::endl;
+	std::cout << "Primer Atributo Valor: " << doc.first_node()->first_attribute()->value() << std::endl;
+	std::cout << "Primer Atributo: " << doc.first_node()->first_attribute("imagePath") << std::endl;
+
+
+
+
+
+
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
 	sf::Texture texture;
 
@@ -18,8 +35,8 @@ void main()
 		sprite.setTexture(texture);
 
 		sprite.setPosition(500, 300);
-		sprite.setRotation(90);
-		sprite.setScale(3, 3);
+		sprite.setRotation(270);
+		sprite.setScale(1, 1);
 
 		while (window.isOpen())
 		{
@@ -31,6 +48,24 @@ void main()
 			}
 			window.clear();
 			window.draw(sprite);
+
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+			{
+				sprite.move(0.1, 0.f);
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+			{
+				sprite.move(-0.1, 0.f);
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+			{
+				sprite.move(0.f, -0.1);
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+			{
+				sprite.move(0.f, 0.1);
+			}
+
 			window.display();
 		}
 	}
