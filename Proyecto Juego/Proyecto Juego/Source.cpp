e008703270c632e73eae52362d3699801ec558b4
zngocti <octi124@hotmail.com>
2017-12-02 08:41:48 -0300

    Implementada la spritesheet

--- Proyecto Juego/Proyecto Juego/Source.cpp
+++ Proyecto Juego/Proyecto Juego/Source.cpp
@@ -10,14 +10,56 @@ void main()
 	rapidxml::xml_document<> doc;
 	doc.parse<0>(xmlFile.data());
 
-	std::cout << "Nombre: " << doc.first_node()->name() << std::endl;
-	std::cout << "Valor: " << doc.first_node()->value() << std::endl;
-	std::cout << "Primer Atributo Nombre: " << doc.first_node()->first_attribute()->name() << std::endl;
-	std::cout << "Primer Atributo Valor: " << doc.first_node()->first_attribute()->value() << std::endl;
-	std::cout << "Primer Atributo: " << doc.first_node()->first_attribute("imagePath") << std::endl;
+	/*std::cout << "X: " << doc.first_node()->first_node()->name() << std::endl;
+	std::cout << "X: " << doc.first_node()->first_node()->first_attribute()->name() << std::endl;
+	std::cout << "X: " << doc.first_node()->first_node()->first_attribute("x")->value() << std::endl;
 
+	std::cout << "X: " << doc.first_node()->first_node()->next_sibling() << std::endl;
+	std::cout << "X: " << doc.first_node()->first_node()->next_sibling()->first_attribute()->name() << std::endl;
+	std::cout << "X: " << doc.first_node()->first_node()->next_sibling()->first_attribute("x")->value() << std::endl;
+	*/
+	rapidxml::xml_node<char>* nodoActual = doc.first_node()->first_node();
+	bool seguirBuscando = true;
+	
+	int spriteX = 0;
+	int spriteY = 0;
+	int spriteH = 0;
+	int spriteW = 0;
 
+	do
+	{
+		if (0 == std::strcmp("ship (6).png", nodoActual->first_attribute("name")->value()))
+		{
+			std::cout << "x: " << nodoActual->first_attribute("x")->value() << std::endl;
+			std::cout << "y: " << nodoActual->first_attribute("y")->value() << std::endl;
+			std::cout << "height: " << nodoActual->first_attribute("height")->value() << std::endl;
+			std::cout << "width: " << nodoActual->first_attribute("width")->value() << std::endl;
 
+			seguirBuscando = false;
+
+			spriteX = strtol(nodoActual->first_attribute("x")->value(), NULL, 0);
+			spriteY = strtol(nodoActual->first_attribute("y")->value(), NULL, 0);
+			spriteH = strtol(nodoActual->first_attribute("height")->value(), NULL, 0);
+			spriteW = strtol(nodoActual->first_attribute("width")->value(), NULL, 0);
+
+			std::cout << "X: " << spriteX << std::endl;
+			std::cout << "Y: " << spriteY << std::endl;
+			std::cout << "H: " << spriteH << std::endl;
+			std::cout << "W: " << spriteW << std::endl;
+
+		}
+		else if (nodoActual == doc.first_node()->last_node())
+		{
+			seguirBuscando = false;
+
+			std::cout << "No se encontro" << std::endl;
+		}
+		else
+		{
+			nodoActual = nodoActual->next_sibling();
+		}
+
+	} while (seguirBuscando);
 
 
 
@@ -25,7 +67,7 @@ void main()
 	sf::Texture texture;
 
 	// cargo un rectangulo de 50x108 que inicia en el (0, 0) del archivo
-	if (!texture.loadFromFile("../Assets/hullLarge (1).png", sf::IntRect(0, 0, 50, 108)))
+	if (!texture.loadFromFile("../Assets/ships_sheet.png", sf::IntRect(spriteX, spriteY, spriteW, spriteH)))
 	{
 		std::cout << "No cargo la imagen" << std::endl;
 	}
